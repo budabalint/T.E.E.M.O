7f806842091d9430ace585c2f3e47ea35aebf7bc
Buda Bálint <buda.balint@students.jedlik.eu>
2026-01-23 20:11:18 +0100

    433 Mhz radio code fixed

--- src/433Mhz/src/main.cpp
+++ src/433Mhz/src/main.cpp
@@ -1,78 +1,89 @@
-#include <LoRa_E220.h>
 #include <Arduino.h>
+#include <LoRa_E220.h>
 
-const uint8_t RADIO_TX = 6;
-const uint8_t RADIO_RX = 5;
-const uint8_t RADIO_M1 = 7;
-const uint8_t RADIO_M0 = 8;
-const uint8_t RADIO_AUX = 4;
+const uint8_t ESP_RX_PIN = 5;
+const uint8_t ESP_TX_PIN = 6;
+const uint8_t AUX_PIN = 4;
+const uint8_t M1_PIN = 7;
+const uint8_t M0_PIN = 8;
 
-const uint8_t DESTINATION_ADDH = 0;
-const uint8_t DESTINATION_ADDL = 2;
-const uint8_t DESTINATION_CHAN = 70;
+const uint8_t SRC_ADDH = 0;
+const uint8_t SRC_ADDL = 1;
+const uint8_t DEST_ADDH = 0;
+const uint8_t DEST_ADDL = 2;
+const uint8_t CHANNEL_433_125 = 23;
 
+LoRa_E220 e220ttl(ESP_RX_PIN, ESP_TX_PIN, &Serial2, AUX_PIN, M0_PIN, M1_PIN, UART_BPS_RATE_9600);
 
-LoRa_E220 e220ttl(&Serial2, RADIO_AUX, RADIO_M0, RADIO_M1);
-
-struct MessageHumidity {
+struct Message {
   char type[5];
-  char message[8];
-  byte humidity;
+  unsigned long count;
+  float val;
 };
 
 void setup() {
-  
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-
   Serial.begin(115200);
-  Serial2.begin(115200, SERIAL_8N1, RADIO_RX, RADIO_TX); 
+  delay(1000);
+
+  pinMode(M0_PIN, OUTPUT);
+  pinMode(M1_PIN, OUTPUT);
+  digitalWrite(M0_PIN, HIGH);
+  digitalWrite(M1_PIN, HIGH);
   delay(1000);
 
   e220ttl.begin();
-  delay(500);
+  delay(200);
+
+  while(Serial2.available()) {
+    Serial2.read();
+  }
 
   ResponseStructContainer c = e220ttl.getConfiguration();
-  
+
   if (c.status.code == E220_SUCCESS) {
       Configuration configuration = *(Configuration*) c.data;
 
-      configuration.ADDH = 0;
-      configuration.ADDL = 1;
-      
-      configuration.SPED.airDataRate = AIR_DATA_RATE_111_625;
-      configuration.SPED.uartBaudRate = UART_BPS_115200;
-      configuration.SPED.uartParity = MODE_00_8N1;
-      configuration.OPTION.transmissionPower = POWER_10;
-      
-      configuration.CHAN = DESTINATION_CHAN;
-      
-      configuration.TRANSMISSION_MODE.enableRSSI = RSSI_ENABLED;
-      configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION;
+      configuration.ADDH = SRC_ADDH;
+      configuration.ADDL = SRC_ADDL;
+      configuration.CHAN = CHANNEL_433_125;
 
-      ResponseStatus rs = e220ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_LOSE);
+      configuration.SPED.airDataRate = AIR_DATA_RATE_000_24;
+      configuration.SPED.uartBaudRate = UART_BPS_9600;
+      configuration.SPED.uartParity = MODE_00_8N1;
+
+      configuration.OPTION.transmissionPower = POWER_10;
+      configuration.OPTION.subPacketSetting = SPS_200_00;
+      configuration.OPTION.RSSIAmbientNoise = RSSI_AMBIENT_NOISE_DISABLED;
+
+      configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED;
+      configuration.TRANSMISSION_MODE.fixedTransmission = FT_FIXED_TRANSMISSION;
+      configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED;
+      configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011;
+
+      ResponseStatus rs = e220ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_SAVE);
       Serial.println(rs.getResponseDescription());
   } else {
-      Serial.print("Kritikus hiba a konfiguracio olvasasakor: ");
       Serial.println(c.status.getResponseDescription());
   }
-  
   c.close();
+
+  e220ttl.setMode(MODE_0_NORMAL);
+  delay(1000);
+  while(Serial2.available()) { Serial2.read(); }
 }
 
 void loop() {
-  MessageHumidity humiMessage = { "HUMI", "Room101", 80};
+  static unsigned long counter = 0;
+  struct Message msg = { "DATA", counter++, 21.5 };
 
-  ResponseStatus rsH = e220ttl.sendFixedMessage(
-    DESTINATION_ADDH, 
-    DESTINATION_ADDL,
-    DESTINATION_CHAN,
-    &humiMessage,
-    sizeof(humiMessage)
+  ResponseStatus rs = e220ttl.sendFixedMessage(
+    DEST_ADDH,
+    DEST_ADDL,
+    CHANNEL_433_125,
+    &msg,
+    sizeof(Message)
   );
 
-  Serial.println(rsH.getResponseDescription());
-  
-  delay(2000); 
+  Serial.println(rs.getResponseDescription());
+  delay(200);
 }
