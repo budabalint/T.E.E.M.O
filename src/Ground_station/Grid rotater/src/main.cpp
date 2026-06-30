b21833355f55bcdc25087d6b01ddc1e02f2e9bff
Buda Bálint <buda.balint@students.jedlik.eu>
2026-07-01 01:32:41 +0200

    grid rotater bug fixing

--- src/Ground_station/Grid rotater/src/main.cpp
+++ src/Ground_station/Grid rotater/src/main.cpp
@@ -23,6 +23,7 @@ struct TelemetryPacket {
   uint8_t Packet_ID;
   float yaw;
   float pitch;
+  uint8_t mag_accuracy;
   uint8_t checksum;
 } __attribute__((packed));
 
@@ -50,7 +51,10 @@ float dynamic_yaw_offset = 0.0;
 bool motors_were_running = false;
 unsigned long last_motor_stop_time = 0;
 
+uint8_t mag_accuracy = 0;
+
 bool global_isManual = true;
+bool tracking_active = false; // Jelzi, ha már kapott parancsot webről vagy joystickról
 bool last_switch_state = false;
 
 uint8_t calculateCRC8(uint8_t *data, uint8_t len) {
@@ -76,22 +80,25 @@ int getAveragedADC(uint8_t pin, int samples = 16) {
 
 void TaskUART(void *pvParameters) {
   for (;;) {
+    // 1. Dobjuk el a memóriában ragadt szemetet, amíg nem találunk egy 0xAA start byte-ot
     while (Serial1.available() > 0 && Serial1.peek() != 0xAA) {
       Serial1.read();
     }
 
-    if (Serial1.available() >= 2) { // Minimum Start marker + ID kell
+    if (Serial1.available() >= 2) { // Kell legalább a Start Marker és az ID
       uint8_t start = Serial1.read();
       uint8_t packet_id = Serial1.read();
       int payload_size = -1;
 
-      if (packet_id == 0x55 || packet_id == 0x66) payload_size = 20; // Alt(4)+Lat(8)+Lon(8)
-      else if (packet_id == 0x33) payload_size = 1;  // Módváltó (1 byte)
-      else if (packet_id == 0x44) payload_size = 9;  // MoveType(1)+Yaw(4)+Pitch(4)
-      else if (packet_id == 0x88) payload_size = 0;  // Csak lekérdezés
+      // Hány byte payload jön az adott parancs után? (CRC NINCS BENNE!)
+      if (packet_id == 0x55 || packet_id == 0x66) payload_size = 20; 
+      else if (packet_id == 0x33) payload_size = 1;  
+      else if (packet_id == 0x44) payload_size = 9;  
+      else if (packet_id == 0x88) payload_size = 0;  // <-- A Te 3 byte-os lekérdezésed (0 payload)
 
       if (payload_size != -1) {
         unsigned long startTime = millis();
+        // Várjuk meg, amíg befut a payload ÉS a +1 byte CRC
         while (Serial1.available() < payload_size + 1) {
           if (millis() - startTime > 100) break; 
           vTaskDelay(1 / portTICK_PERIOD_MS);
@@ -99,9 +106,9 @@ void TaskUART(void *pvParameters) {
 
         if (Serial1.available() >= payload_size + 1) {
           uint8_t buffer[32];
-          buffer[0] = start;
-          buffer[1] = packet_id;
-          Serial1.readBytes(&buffer[2], payload_size + 1);
+          buffer[0] = start;       // 0xAA
+          buffer[1] = packet_id;   // 0x88
+          Serial1.readBytes(&buffer[2], payload_size + 1); // CRC beolvasása
 
           uint8_t calculatedCRC = calculateCRC8(buffer, payload_size + 2);
           uint8_t receivedCRC = buffer[payload_size + 2];
@@ -110,18 +117,15 @@ void TaskUART(void *pvParameters) {
             xSemaphoreTake(dataMutex, portMAX_DELAY);
             lastUARTDataTime = millis();
             
+            // --- Csomagok feldolgozása ---
             if (packet_id == 0x55 || packet_id == 0x66) {
               float p_alt; double p_lat, p_lon;
               memcpy(&p_alt, &buffer[2], 4);
               memcpy(&p_lat, &buffer[6], 8);
               memcpy(&p_lon, &buffer[14], 8);
-
               if (p_lat != 0.0 && p_lon != 0.0) {
-                if (packet_id == 0x55) {
-                  p_alt; target_lat = p_lat; target_lon = p_lon;
-                } else {
-                  tracker_alt = p_alt; tracker_lat = p_lat; tracker_lon = p_lon;
-                }
+                if (packet_id == 0x55) { target_alt = p_alt; target_lat = p_lat; target_lon = p_lon; } 
+                else { tracker_alt = p_alt; tracker_lat = p_lat; tracker_lon = p_lon; }
               }
             } 
             else if (packet_id == 0x33) {
@@ -133,30 +137,38 @@ void TaskUART(void *pvParameters) {
               memcpy(&y_val, &buffer[3], 4);
               memcpy(&p_val, &buffer[7], 4);
 
-              if (move_type == 0) {
-                target_yaw = y_val;
-                target_pitch = p_val;
-              } else {
-                target_yaw = current_yaw + y_val;
-                target_pitch = current_pitch + p_val;
-                while(target_yaw >= 360.0) target_yaw -= 360.0;
-                while(target_yaw < 0.0) target_yaw += 360.0;
-              }
+              if (move_type == 0) { target_yaw = y_val; target_pitch = p_val; } 
+              else { target_yaw = current_yaw + y_val; target_pitch = current_pitch + p_val; }
+              
+              while(target_yaw >= 360.0) target_yaw -= 360.0;
+              while(target_yaw < 0.0) target_yaw += 360.0;
+              
               global_isManual = true;
+              tracking_active = true; // Élesedik a követés!
             }
+            // (Ha a packet_id 0x88, nincs más teendő, mint válaszolni)
 
+            // --- VÁLASZ KÜLDÉSE ---
+            // Bármilyen érvényes csomag jön (köztük a 0x88 lekérdezés is), visszaküldjük a telemetriát!
             TelemetryPacket txPacket;
             txPacket.startMarker = 0xBB;
             txPacket.Packet_ID = 0x77;
             txPacket.yaw = current_yaw;
             txPacket.pitch = current_pitch;
+            txPacket.mag_accuracy = mag_accuracy;
             xSemaphoreGive(dataMutex);
 
             txPacket.checksum = calculateCRC8((uint8_t*)&txPacket, sizeof(TelemetryPacket) - 1);
+            
+            // Adat kiküldése és hardveres puffer kényszerített ürítése
             Serial1.write((uint8_t*)&txPacket, sizeof(TelemetryPacket));
+            Serial1.flush(); 
+
+          } else {
+            // Hibakereséshez: ha rossz a számítás, ezt látod a konzolban
+            Serial.printf("UART Hiba: CRC nem egyezik! Kapott: %02X, Szamolt: %02X\n", receivedCRC, calculatedCRC);
           }
         }
-      } else {
       }
     }
     vTaskDelay(10 / portTICK_PERIOD_MS);
@@ -187,6 +199,7 @@ void TaskSensor(void *pvParameters) {
       xSemaphoreTake(dataMutex, portMAX_DELAY);
       if (sensorValue.sensorId == SH2_ROTATION_VECTOR) {
         mag_yaw = raw_yaw;
+        mag_accuracy = sensorValue.status;
       } 
       else if (sensorValue.sensorId == SH2_GAME_ROTATION_VECTOR) {
         game_yaw = raw_yaw;
@@ -201,7 +214,6 @@ void TaskSensor(void *pvParameters) {
 
 void TaskControl(void *pvParameters) {
   static unsigned long lastDebugPrint = 0;
-  static bool first_run = true;
 
   for (;;) {
     bool current_switch = (digitalRead(MODE_SELECTER_BUTTON) == HIGH);
@@ -216,12 +228,10 @@ void TaskControl(void *pvParameters) {
     while (current_yaw >= 360.0) current_yaw -= 360.0;
     while (current_yaw < 0.0) current_yaw += 360.0;
 
-    // if (first_run && (current_yaw != 0.0 || current_pitch != 0.0)) {
-    //     target_yaw = current_yaw;
-    //     target_pitch = current_pitch;
-    //     first_run = false;
-    // }
-
+    if (!tracking_active) {
+            target_yaw = current_yaw;
+            target_pitch = current_pitch;
+        }
     double t_lat = target_lat;
     double t_lon = target_lon;
     float t_alt = target_alt;
@@ -246,49 +256,92 @@ void TaskControl(void *pvParameters) {
     bool r1 = LOW, r2 = LOW, r3 = LOW, r4 = LOW;
 
     if (isManual) {
+      // int adc_val = getAveragedADC(ANALOG_BUTTON, 16);
+      // bool btn_left = false, btn_right = false, btn_up = false, btn_down = false;
+      // // 510, 541, 621, 1548, 1677, 1715, 2013, 2866
+      // if (adc_val > 400 && adc_val <= 525) { 
+      //   // Jobboldal
+      //   btn_down = true; 
+      //   btn_right = true; 
+      // } 
+      // else if (adc_val > 525 && adc_val <= 580) { 
+      //   // Bal felső + Jobb alsó
+      //   btn_up = true; 
+      //   btn_right = true; 
+      // } 
+      // else if (adc_val > 580 && adc_val <= 1085) { 
+      //   // Jobb alsó
+      //   btn_right = true; 
+      // } 
+      // else if (adc_val > 1085 && adc_val <= 1612) { 
+      //   // Jobb felső + Bal alsó
+      //   btn_down = true; 
+      //   btn_left = true; 
+      // } 
+      // else if (adc_val > 1612 && adc_val <= 1696) { 
+      //   // Jobb felső
+      //   btn_down = true; 
+      // } 
+      // else if (adc_val > 1696 && adc_val <= 1864) { 
+      //   // Baloldal
+      //   btn_up = true; 
+      //   btn_left = true; 
+      // } 
+      // else if (adc_val > 1864 && adc_val <= 2440) { 
+      //   // Bal felső
+      //   btn_up = true; 
+      // } 
+      // else if (adc_val > 2440 && adc_val <= 3300) { 
+      //   // Bal alsó
+      //   btn_left = true; 
+      // }
       int adc_val = getAveragedADC(ANALOG_BUTTON, 16);
       bool btn_left = false, btn_right = false, btn_up = false, btn_down = false;
-      // 510, 541, 621, 1548, 1677, 1715, 2013, 2866
-      if (adc_val > 400 && adc_val <= 525) { 
-        // Jobboldal
+
+      // Határértékek kiszámolva a mért értékek (545, 627, 661, 1255, 1752, 1791, 2103, 2974) közé:
+      if (adc_val > 400 && adc_val <= 586) { 
+        // Mért érték: 545 -> 2 jobb egyszerre (Csak Jobbra)
+        btn_right = true; 
+      } 
+      else if (adc_val > 586 && adc_val <= 644) { 
+        // Mért érték: 627 -> ket also (Csak Lefelé)
+        btn_down = true; 
+      } 
+      else if (adc_val > 644 && adc_val <= 958) { 
+        // Mért érték: 661 -> jobb also (Jobbra + Lefelé)
         btn_down = true; 
         btn_right = true; 
       } 
-      else if (adc_val > 525 && adc_val <= 580) { 
-        // Bal felső + Jobb alsó
+      else if (adc_val > 958 && adc_val <= 1503) { 
+        // Mért érték: 1255 -> ket folso (Csak Felfelé)
+        btn_up = true; 
+      } 
+      else if (adc_val > 1503 && adc_val <= 1771) { 
+        // Mért érték: 1752 -> jobb felso (Jobbra + Felfelé)
         btn_up = true; 
         btn_right = true; 
       } 
-      else if (adc_val > 580 && adc_val <= 1085) { 
-        // Jobb alsó
-        btn_right = true; 
-      } 
-      else if (adc_val > 1085 && adc_val <= 1612) { 
-        // Jobb felső + Bal alsó
-        btn_down = true; 
+      else if (adc_val > 1771 && adc_val <= 1947) { 
+        // Mért érték: 1791 -> 2 bal egyszerre (Csak Balra)
         btn_left = true; 
       } 
-      else if (adc_val > 1612 && adc_val <= 1696) { 
-        // Jobb felső
-        btn_down = true; 
-      } 
-      else if (adc_val > 1696 && adc_val <= 1864) { 
-        // Baloldal
+      else if (adc_val > 1947 && adc_val <= 2538) { 
+        // Mért érték: 2103 -> bal fölső (Balra + Felfelé)
         btn_up = true; 
         btn_left = true; 
       } 
-      else if (adc_val > 1864 && adc_val <= 2440) { 
-        // Bal felső
-        btn_up = true; 
-      } 
-      else if (adc_val > 2440 && adc_val <= 3300) { 
-        // Bal alsó
+      else if (adc_val > 2538 && adc_val <= 3500) { 
+        // Mért érték: 2974 -> bal also (Balra + Lefelé)
+        btn_down = true; 
         btn_left = true; 
       }
-
       if (btn_left || btn_right || btn_up || btn_down) {
-        r1 = btn_left; r2 = btn_right; r3 = btn_up; r4 = btn_down;
-        
+        // r1 = btn_left; r2 = btn_right; r3 = btn_up; r4 = btn_down;
+        r1 = btn_right; 
+        r2 = btn_left;  
+        r3 = btn_up; 
+        r4 = btn_down;
+        tracking_active = true;
         xSemaphoreTake(dataMutex, portMAX_DELAY);
         target_yaw = c_yaw;
         target_pitch = c_pitch;
@@ -358,9 +411,12 @@ void TaskControl(void *pvParameters) {
 
         if (millis() - last_motor_stop_time > 1000) {
             xSemaphoreTake(dataMutex, portMAX_DELAY);
-            dynamic_yaw_offset = mag_yaw - game_yaw;
-            while (dynamic_yaw_offset > 180.0) dynamic_yaw_offset -= 360.0;
-            while (dynamic_yaw_offset < -180.0) dynamic_yaw_offset += 360.0;
+          if (mag_accuracy >= 2) {
+              float new_offset = mag_yaw - game_yaw;
+              while (new_offset > 180.0) new_offset -= 360.0;
+              while (new_offset < -180.0) new_offset += 360.0;
+              dynamic_yaw_offset = new_offset;
+            }
             xSemaphoreGive(dataMutex);
         }
     }
@@ -411,7 +467,7 @@ void setup() {
   delay(500);
 
   bool bno_ok = false;
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 8; i++) {
     if (bno08x.begin_I2C(0x4A, &Wire)) {
       bno_ok = true;
       Serial.println("BNO08x sikeresen csatlakozott!");
@@ -420,12 +476,14 @@ void setup() {
     Serial.println("BNO init hiba, ujraprobalkozas 500ms mulva...");
     delay(500);
   }
-
-  if (!bno08x.begin_I2C(0x4A, &Wire)) {
-    Serial.println("Hiba a BNO szenzorral!");
-    for(int i = 0; i < LED_COUNT; i++) strip.setPixelColor(i, strip.Color(255, 0, 0)); 
+  if (!bno_ok) {
+    Serial.println("Szar a bno (Nem talalhato az I2C cimen 8 probalkozas utan sem)!");
+    for(int i = 0; i < LED_COUNT; i++) strip.setPixelColor(i, strip.Color(255, 0, 0)); // Váltsuk pirosra a ledet hiba esetén
     strip.show();
-    while (1) { delay(10); }
+    
+    while (1) { 
+      delay(10);
+    }
   }
 
   bno08x.enableReport(SH2_ROTATION_VECTOR);
